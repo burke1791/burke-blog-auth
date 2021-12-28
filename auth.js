75c7979b4ca50844c6bf387cd306e67b7b545893
Chris Burke <burke1791@gmail.com>
2021-12-27 22:49:08 -0600

    refactored to match new version of simple-oauth

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const simpleOauthModule = require('simple-oauth2');
+const { AuthorizationCode } = require('simple-oauth2');
 const randomstring = require('randomstring');
 const Secrets = require('./lib/secrets');
 
@@ -30,83 +30,73 @@ function getScript(mess, content) {
   </script></body></html>`;
 }
 
-module.exports.auth = (e, ctx, cb) => secrets.init()
-  .then(() => {
-    console.log(process.env);
-    console.log(process.env.GIT_HOSTNAME);
-    const oauth2 = simpleOauthModule.create({
-      client: {
-        id: secrets.OAUTH_CLIENT_ID,
-        secret: secrets.OAUTH_CLIENT_SECRET,
-      },
-      auth: {
-        tokenHost: secrets.GIT_HOSTNAME,
-        tokenPath: secrets.OAUTH_TOKEN_PATH,
-        authorizePath: secrets.OAUTH_AUTHORIZE_PATH,
-      },
-    });
-
-    // Authorization uri definition
-    const authorizationUri = oauth2.authorizationCode.authorizeURL({
-      redirect_uri: secrets.REDIRECT_URL,
-      scope: secrets.OAUTH_SCOPES,
-      state: randomstring.generate(32),
-    });
-
-    cb(null, {
-      statusCode: 302,
-      headers: {
-        Location: authorizationUri,
-      },
-    });
+module.exports.auth = (e, ctx, cb) => secrets.init().then(() => {
+  const oauth2 = new AuthorizationCode({
+    client: {
+      id: secrets.OAUTH_CLIENT_ID,
+      secret: secrets.OAUTH_CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: secrets.GIT_HOSTNAME,
+      tokenPath: secrets.OAUTH_TOKEN_PATH,
+      authorizePath: secrets.OAUTH_AUTHORIZE_PATH,
+    },
   });
 
-module.exports.callback = (e, ctx, cb) => {
-  let oauth2;
-  secrets.init()
-    .then(() => {
-      oauth2 = simpleOauthModule.create({
-        client: {
-          id: secrets.OAUTH_CLIENT_ID,
-          secret: secrets.OAUTH_CLIENT_SECRET,
-        },
-        auth: {
-          tokenHost: secrets.GIT_HOSTNAME,
-          tokenPath: secrets.OAUTH_TOKEN_PATH,
-          authorizePath: secrets.OAUTH_AUTHORIZE_PATH,
-        },
-      });
+  // Authorization uri definition
+  const authorizationUri = oauth2.authorizeURL({
+    redirectURI: secrets.REDIRECT_URL,
+    scope: secrets.OAUTH_SCOPES,
+    state: randomstring.generate(32),
+  });
 
-      const options = {
-        code: e.queryStringParameters.code,
-      };
-      return oauth2.authorizationCode.getToken(options);
-    })
-    .then((result) => {
-      const token = oauth2.accessToken.create(result);
-      cb(
-        null,
-        {
-          statusCode: 200,
-          headers: {
-            'Content-Type': 'text/html',
-          },
-          body: getScript('success', {
-            token: token.token.access_token,
-            provider: 'github',
-          }),
-        },
-      );
-    })
-    .catch((err) => {
-      cb(null, {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'text/html',
-        },
-        body: getScript('error', err),
-      });
+  cb(null, {
+    statusCode: 302,
+    headers: {
+      Location: authorizationUri,
+    },
+  });
+});
+
+module.exports.callback = (e, ctx, cb) => {
+  const oauth2 = new AuthorizationCode({
+    client: {
+      id: secrets.OAUTH_CLIENT_ID,
+      secret: secrets.OAUTH_CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: secrets.GIT_HOSTNAME,
+      tokenPath: secrets.OAUTH_TOKEN_PATH,
+      authorizePath: secrets.OAUTH_AUTHORIZE_PATH,
+    },
+  });
+
+  secrets.init().then(() => {
+    const options = {
+      code: e.queryStringParameters.code,
+    };
+    return oauth2.getToken(options);
+  }).then((result) => {
+    const token = oauth2.createToken(result);
+    cb(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: getScript('success', {
+        token: token.token.access_token,
+        provider: 'github',
+      })
     });
+  }).catch((err) => {
+    cb(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: getScript('error', err),
+    });
+  });
 };
 
 module.exports.success = (e, ctx, cb) => cb(
